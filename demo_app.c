8b8d86873d51f381e44b0e09abb7644d5a11de6d
hedger <hedger@nanode.su>
2023-04-10 19:47:03 +0400

    matrix

--- demo_app.c
+++ demo_app.c
@@ -6,6 +6,10 @@
 int32_t demo_app_app(void* p) {
     UNUSED(p);
     FURI_LOG_I("TEST", "Hello world");
+    for(int i = 0; i < 10; i++) {
+        FURI_LOG_I("TEST", "i = %d", i);
+        furi_delay_ms(10000);
+    }
     FURI_LOG_I("TEST", "I'm demo_app!");
 
     return 0;
